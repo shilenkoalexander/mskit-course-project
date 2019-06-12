fa27fc7ea8ab48a93f10c736ca5f2a61fd9bd3b7
sasha <sashazemzer@gmail.com>
2019-06-13 00:32:46 +0300

    начал оптимизацию

--- src/main/java/org/donntu/knt/mskit/course/myfilters/Filter.java
+++ src/main/java/org/donntu/knt/mskit/course/myfilters/Filter.java
@@ -14,25 +14,48 @@ public class Filter {
         int[][] newPixels = new int[normalHeight][normalWidth];
         int blurMatrixSize = 2 * radius + 1;
 
-        double[][] blurMatrix = getKernel(blurMatrixSize, kernelValue);
+        double[] blurKernel = getKernel(blurMatrixSize, kernelValue);
 
         int[][] extendMatrix = extendMatrix(pixels, radius);
         for (int i = 0; i < normalHeight; i++) {
             for (int j = 0; j < normalWidth; j++) {
-                newPixels[i][j] = getBlurredPixel(
-                        extendMatrix,
-                        blurMatrix,
-                        i + radius,
-                        j + radius,
-                        radius
-                );
+                for (int k = 0; k < 2; k++) {
+                    newPixels[i][j] = getBlurredPixel(
+                            extendMatrix,
+                            blurKernel,
+                            i + radius,
+                            j + radius,
+                            radius,
+                            k == 0
+                    );
+                }
             }
         }
 
         return newPixels;
     }
 
-    private int getBlurredPixel(int[][] extendMatrix, double[][] blurMatrix, int i, int j, int radius) {
+    private int getBlurredPixel(int[][] extendMatrix, double[] blurKernel, int i, int j, int radius, boolean vertical) {
+        double sumR = 0;
+        double sumG = 0;
+        double sumB = 0;
+        for (int k = i - radius, p = 0; k < i + radius + 1; k++, p++) {
+            int rgb;
+            if (vertical) {
+                rgb = extendMatrix[k][j];
+            } else {
+                rgb = extendMatrix[i][k];
+            }
+            Color color = new Color(rgb);
+            sumR += (color.getRed() * blurKernel[p]);
+            sumG += (color.getGreen() * blurKernel[p]);
+            sumB += (color.getBlue() * blurKernel[p]);
+        }
+
+        return new Color((int) Math.round(sumR), (int) Math.round(sumG), (int) Math.round(sumB)).getRGB();
+    }
+
+    /*private int getBlurredPixel(int[][] extendMatrix, double[] blurMatrix, int i, int j, int radius, boolean vertical) {
         double sumR = 0;
         double sumG = 0;
         double sumB = 0;
@@ -45,30 +68,30 @@ public class Filter {
             }
         }
 
-        return new Color((int)Math.round(sumR), (int)Math.round(sumG), (int)Math.round(sumB)).getRGB();
-    }
+        return new Color((int) Math.round(sumR), (int) Math.round(sumG), (int) Math.round(sumB)).getRGB();
+    }*/
 
-    private double[][] getKernel(int size, KernelValue kernelValue) {
-        double[][] blurMatrix = new double[size][size];
+    private double[] getKernel(int size, KernelValue kernelValue) {
+        double[] blurKernel = new double[size];
         int half = size / 2;
         double sum = 0;
         for (int i = 0; i < size; i++) {
+            double rowSum = 0.0;
             for (int j = 0; j < size; j++) {
-                blurMatrix[i][j] = kernelValue.getValue(i - half, j - half);
-                sum += blurMatrix[i][j];
-//                System.out.printf("%4.3f ", blurMatrix[i][j]);
+                rowSum += kernelValue.getValue(i - half, j - half);
+//                System.out.printf("%4.3f ", blurKernel[i][j]);
             }
+            blurKernel[i] = rowSum;
+            sum += rowSum;
 //            System.out.println();
         }
 
-//        double sumNormal = 0;
+        double sumNormal = 0;
         for (int i = 0; i < size; i++) {
-            for (int j = 0; j < size; j++) {
-                blurMatrix[i][j] /= sum;
-//                sumNormal += blurMatrix[i][j];
-            }
+            blurKernel[i] /= sum;
+            sumNormal += blurKernel[i];
         }
-//        System.out.println("sum = " + sumNormal);
-        return blurMatrix;
+        System.out.println("sum = " + sumNormal);
+        return blurKernel;
     }
 }
